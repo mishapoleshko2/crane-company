202213e852f599dbeef59b141dec19f04c96f672
poleshkomi <poleshkomi@modeltech.ru>
2025-01-11 16:37:58 +0300

    added metadata to Base

--- crane_company/infra/sqlalchemy_db/db.py
+++ crane_company/infra/sqlalchemy_db/db.py
@@ -1,10 +1,22 @@
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declarative_base
 
 from crane_company.settings import settings
 
 
-class Base(AsyncAttrs, DeclarativeBase): ...
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    metadata = metadata
 
 
 engine = create_async_engine(str(settings.db_uri))
